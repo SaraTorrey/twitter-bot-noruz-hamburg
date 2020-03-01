e41dfb25a7592f396f795ec2863f34afefdcc434
Sara Torrey <sara.moravej@gmail.com>
2020-03-01 14:06:03 +0100

    Add Spring project structure

---
+++ src/main/java/org/saratorrey/twitter/bot/Hamburg/Noruz/TwitterBotHamburgNoruzApplication.java
@@ -0,0 +1,13 @@
+package org.saratorrey.twitter.bot.Hamburg.Noruz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterBotHamburgNoruzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterBotHamburgNoruzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/saratorrey/twitter/bot/Hamburg/Noruz/TwitterBotHamburgNoruzApplicationTests.java
@@ -0,0 +1,13 @@
+package org.saratorrey.twitter.bot.Hamburg.Noruz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterBotHamburgNoruzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
